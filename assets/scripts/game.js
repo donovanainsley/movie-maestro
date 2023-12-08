06f59b1bdb9ecb4ff22993736aeefb09d4bd7c5b
donovanainsley <donovan_weise@live.co.uk>
2023-12-08 14:08:36 +0000

    js:added constants for IDs located in game.html

---
+++ assets/scripts/game.js
@@ -0,0 +1,5 @@
+// Constants for accessing elements
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progress-text");
+const scoreText = document.getElementById("score");

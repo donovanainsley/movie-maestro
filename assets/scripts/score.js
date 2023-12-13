f4ba8db1df7727cf6029a56554f2c21c62ffb82d
donovanainsley <donovan_weise@live.co.uk>
2023-12-13 17:43:54 +0000

    created score.js to display the list of high scores in scores.html

---
+++ assets/scripts/score.js
@@ -0,0 +1,17 @@
+//Constant to access high-score-list.
+const highScoresList = document.getElementById("high-score-list")
+
+//Retrieve highScores from local storage.
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []
+
+//Setting the html content of highScoresList using retrieved high scores.
+highScoresList.innerHTML = 
+
+  //Mapping each high score to an HTML list item.
+  highScores.map(score => {
+
+    //Creates a list item with the class high-score and displays the users name and score
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  //Joins the list items into a string to set as the innerHTML of highScoresList.
+  .join("");

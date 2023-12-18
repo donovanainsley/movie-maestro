e49b32b68b1787882e54fb3da00937c2b0d3897e
donovanainsley <donovan_weise@live.co.uk>
2023-12-18 16:45:56 +0000

    added semi-colons and removed MAX_HIGH_SCORES variable from end.js and added validator result to testing folder

--- assets/scripts/end.js
+++ assets/scripts/end.js
@@ -7,47 +7,47 @@ const finalScore  = document.getElementById("final-score");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 //Retrieve high scores from local storage or create an empty array.
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 // Constant for the maximum number of high scores to be stored.
-const MAX_HIGH_SCORES = 5
+// const MAX_HIGH_SCORES = 5
 
 //Displays most recent scores on the page.
-finalScore.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore;
 
 //Event listener for username input.
 username.addEventListener('keyup', () => {
 
     //Disables save score button if username field is empty.
-    saveScore.disabled = !username.value
+    saveScore.disabled = !username.value;
 });
 
 //Function to save high score.
  saveHighScore = e => {
 
     //Prevents default form submission behavior.
-    e.preventDefault ()
+    e.preventDefault ();
 
     //Creates an object with the current score and username.
     const score = {
         score: mostRecentScore,
         name: username.value
-    }
+    };
 
     //Adds new score object to to highScores array.
-    highScores.push(score)
+    highScores.push(score);
 
     //Sorts highScores array in a descending order based on score.
     highScores.sort((a,b)=> {
-        return b.score - a.score
-    })
+        return b.score - a.score;
+    });
 
     //Truncating the array to keep only the top MAX_HIGH_SCORES.
-    highScores.splice(5)
+    highScores.splice(5);
 
     //Saves the updated highScores array to local storage.
-    localStorage.setItem('highScores', JSON.stringify(highScores))
+    localStorage.setItem('highScores', JSON.stringify(highScores));
 
     //Redirects user to scores.html page.
-    window.location.assign('scores.html')
- } 
+    window.location.assign('scores.html');
+ };
